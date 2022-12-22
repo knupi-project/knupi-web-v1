c2a46e599953df153c8d9b3f52cb6700c62c79a4
seunmul <geonhab504@gmail.com>
2022-12-22 23:18:42 +0900

    #1 GeonhaPark | reservation page temp save

---
+++ src/components/ReserveStatus.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReserveStatus = () => {
+  return (
+    <div>ReserveStatus</div>
+  )
+}
+
+export default ReserveStatus
--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -8,12 +8,15 @@ const Footer = () => {
       <div className="footer__menu">
         <div className="footer__menu__item">
           <img src={process.env.PUBLIC_URL + '/img/message.png'} alt="img" />
+          <p>knupi2022@naver.com</p>
         </div>
         <div className="footer__menu__item">
           <img src={process.env.PUBLIC_URL + '/img/call.png'} alt="img" />
+          <p>010-9985-8584 (회장 : 조연호)</p>
         </div>
         <div className="footer__menu__item">
           <img src={process.env.PUBLIC_URL + '/img/insta.png'} alt="img" />
+          <p>@knupi_official</p>
         </div>
       </div>
       <div className="footer__copyright">
