397e776cdda48b2c5312c01e841bc2e20d1829a7
Yeonho Cho <nanyeonho@gmail.com>
2023-01-08 14:28:10 +0900

    1/8 달력 완료

--- src/routes/ReserveComplete.js
+++ src/routes/ReserveComplete.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { auth } from 'util/firebaseConfig';
 import { Link } from 'react-router-dom';
 const Reservecheck = () => {
   const selectedTime = localStorage.getItem('key');
