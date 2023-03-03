205504013908b0be3b9eb39c86dee42f37c1f92f
Yeonho Cho <nanyeonho@gmail.com>
2023-03-03 17:02:32 +0900

    현재 시간 이전 선택지는 삭제
    시간을 수식으로 인덱스화 해서 타임 어레이 모두 업데이트 한 이후 리저브[0] 이 현재 시간보다 이전이면 삭제

--- src/views/reserve/components/TimeArray.js
+++ src/views/reserve/components/TimeArray.js
@@ -29,6 +29,13 @@ const TimeArray = ({ startDate, type, reserveArray }) => {
     return timeStr;
   }
 
+  function getIndexFromTime(time) {
+    const hour = time.getHours();
+    const min = time.getMinutes();
+    const index = (hour - 9) * 2 + (min === '30' ? 1 : 0);
+    return index;
+  }
+
   function getTimeArray(start, end, interval) {
     let startTime = getTimeFromHourMinuteString(start);
     const endTime = getTimeFromHourMinuteString(end);
@@ -47,6 +54,13 @@ const TimeArray = ({ startDate, type, reserveArray }) => {
     return timeArray;
   }
   const timeArray = getTimeArray('09:00', '22:30', 30);
+
+  const today = new Date();
+  if (startDate.getDate() == today.getDate()) {
+    while (getIndexFromTime(timeArray[0]) <= getIndexFromTime(new Date())) {
+      timeArray.splice(0, 1);
+    }
+  }
 
   return (
     <>
