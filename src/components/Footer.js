cf9af8d69061f79142385c05177a1f25cbe556c1
yeonh0 <nanyeonho@gmail.com>
2025-03-30 03:06:10 +0900

    Footer 수정 - 구성 변경 - 크기 조정 - 배경 색 조정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,25 +13,19 @@ const Footer = () => {
             window.scrollTo({ top: 0, behavior: 'smooth' });
           }}
         >
-          <div className="footer_logo">
-            <img
-              className="footer_logo_duck"
-              src={process.env.PUBLIC_URL + '/img/logo_duck_white.png'}
-              alt="img"
-            />
-            <div className="footer_logo_text">
-              <span className="footer_logo__title">KNUPI</span>
-              <span className="footer_logo__subtitle">KNU PIANO CLUB</span>
+          <div className="footer_name">
+            <div className="fotter_logotitle">
+              <img
+                className="footer_logo_duck"
+                src={process.env.PUBLIC_URL + '/img/logo_duck_white.png'}
+                alt="img" />
+              <p className="footer_name_title"> KNUPI | We Are KNUPI</p>
             </div>
+            <p className="footer_name_copyright">
+              © KNUPI | 2023 경북대학교 중앙동아리 크누피
+            </p>
           </div>
         </Link>
-      </div>
-
-      <div className="footer_name">
-        <p className="footer_name_title"> KNUPI | We Are KNUPI</p>
-        <p className="footer_name_copyright">
-          © KNUPI | 2023 경북대학교 중앙동아리 크누피
-        </p>
       </div>
     </div>
   );
